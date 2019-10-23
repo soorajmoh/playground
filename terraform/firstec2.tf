c7426a159d0a9bbcd119debfc0cae82fb85fec1e
Sooraj Mohammed <soorajmoh@gmail.com>
2019-10-23 00:08:41 -0700

    ec2 instance create via terraform

--- terraform/firstec2.tf
+++ terraform/firstec2.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1b"
+  region = "us-east-1"
   access_key = "AKIA57CL2DT5HUPBVDCF"
   secret_key = "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"
 }
@@ -8,5 +8,5 @@ resource "aws_instance" "ub1804-tf1" {
   ami = "ami-04b9e92b5572fa0d1"
   instance_type = "t2.micro"
   key_name = "docker-testserver-1"
-  security_groups =["All-Traffic-open"]
+  security_groups = ["All-Traffic-open"]
 }
